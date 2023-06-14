bcb48f653d90f6f5e1de28fb214715c416330d94
Patryk <117048649+Vincerre@users.noreply.github.com>
2023-06-14 18:50:50 +0200

    task 27.4 completed

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('express-handlebars');
+const multer = require('multer');
+
+const upload = multer();
 
 const app = express();
 app.engine('hbs', hbs({ extname: 'hbs' }));
 app.set('view engine', 'hbs');
 
-// wyłącza błąd dla handlebars
-// app.locals.layout = false;
-
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', (req, res) => {
@@ -33,6 +35,16 @@ app.get('/history', (req, res) => {
 
 app.get('/hello/:name', (req, res) => {
   res.render('hello', { name: req.params.name });
+});
+
+app.post('/contact/send-message', upload.single('image'), (req, res) => {
+  const { author, sender, title, message } = req.body;
+  const image = req.file;
+  if (author && sender && title && message && image) {
+    res.render('contact', { isSent: true, image: image.originalname });
+  } else {
+    res.render('contact', { isError: true });
+  }
 });
 
 app.use((req, res) => {
